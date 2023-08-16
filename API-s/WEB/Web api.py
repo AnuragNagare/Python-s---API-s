be4e4ea1c4d557de09710bcdf22cdf792ae01e13
Anurag Ramesh Nagare <85473989+AnuragNagare@users.noreply.github.com>
2023-08-16 19:44:01 +0530

    Update Web api.py

--- API-s/WEB/Web api.py
+++ API-s/WEB/Web api.py
@@ -9,7 +9,7 @@ import json
 import urllib.parse
 import requests
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "Your Api key"
 OPENAI_API_ENDPOINT = "https://api.openai.com/v1/engines/davinci/completions"
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
